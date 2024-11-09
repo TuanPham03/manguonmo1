52aba8570fe17e229650fff558aa473394b375b8
TuanPham03 <manhtuanpham68@gmail.com>
2024-11-09 11:24:45 +0700

    sochinhphuong

--- index.php
+++ index.php
@@ -6,8 +6,37 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    
+        
+        <?php
         echo "hello";
-    ?>
+        function kiemTraSoChinhPhuong($n) {
+            
+            if ($n < 0) {
+                return false;
+            }
+        
+            
+            $sqrt = sqrt($n);
+        
+         
+            if ($sqrt == floor($sqrt)) {
+                return true; 
+            } else {
+                return false; 
+            }
+        }
+        
+      
+        $n = 16;
+        if (kiemTraSoChinhPhuong($n)) {
+            echo "$n là số chính phương.";
+        } else {
+            echo "$n không phải là số chính phương.";
+        }
+
+?>
+        
+
 </body>
 </html>
