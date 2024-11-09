03a2bc8577cc6bdf1ae8adc48cf81845be37565b
TranThanhF <phuongsd2016@gmail.com>
2024-11-09 11:29:55 +0700

    phuong

--- index.php
+++ index.php
@@ -9,5 +9,26 @@
     <?php 
         echo "hello";
     ?>
+    <?php
+function kiemTraSoNguyenTo($n) {
+    if ($n <= 1) {
+        return false; 
+    }
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+$so = 29;
+if (kiemTraSoNguyenTo($so)) {
+    echo $so . " La so nguyen to";
+} else {
+    echo $so . " Khong phai so nguyen to";
+}
+?>
+
 </body>
 </html>
